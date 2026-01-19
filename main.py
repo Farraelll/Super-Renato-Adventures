7d36d3f545471e521efb0a4969b6ee086d54a935
JoaoVitorOliveiraScheidegger <joao.scheidegger@gmail.com>
2026-01-19 14:41:21 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame as pg
+
+pg.init()
+janela = pg.display.set_mode((800,600))
+pg.display.set_caption("Teste")
+clock = pg.time.Clock()
+
+crashed = False
+
+while not crashed:
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            crashed = True
+
+    pg.display.flip()
+
+pg.quit()
